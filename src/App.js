e6faf3ddf13d737bdaa2316b09bde75285d7c611
Arturo Gómez <artuedu@gmail.com>
2020-07-09 20:06:35 -0500

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Box extends Component{
+  render(){
+    return(
+      <div style={{border: '1px solid #097', margin: 5, padding: 5}}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class Article extends Component{
+
+  static propTypes = {
+    author: PropTypes.string.isRequired
+  }
+
+  render(){
+    const {author, children, date, title} = this.props;
+
+    return(
+      <section>
+        <h2>{title}</h2>
+        {author && <p><em>Escrito por {author}</em></p>}
+        <Box>{date}</Box>
+        <article>
+          {children}
+        </article>
+      </section>
+    );
+  }
+}
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <h4>Children props</h4>
+        <Article title="Articulo soblre la prop children" author="Arturo"  date={new Date().toLocaleDateString()}>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, modi. Assumenda neque accusantium ducimus enim ex recusandae commodi tenetur, amet totam, voluptate doloremque facere pariatur minus quisquam optio necessitatibus officiis.</p>
+        </Article>
+      </div>
+    );
+  }
 }
 
 export default App;
